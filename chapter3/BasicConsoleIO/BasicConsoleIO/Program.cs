5d2c7d7d9dac2cbaa3b2d725107af901668f9a40
diwashrestha <diwash712@gmail.com>
2020-06-03 22:45:08 +0545

    add tryparse

--- chapter3/BasicConsoleIO/BasicConsoleIO/Program.cs
+++ chapter3/BasicConsoleIO/BasicConsoleIO/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BasicConsoleIO
 {
@@ -15,8 +16,44 @@ namespace BasicConsoleIO
             ObjectFunctionality();
             DataTypeFunctionality();
             CharFunctionality();
+            ParseFromStrings();
+            ParseFromStringsWithTryParse();
             Console.ReadLine();
 
+        }
+
+        private static void ParseFromStringsWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b));
+            {
+                Console.WriteLine("Value of b: {0}",b);
+            }
+
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}",d);
+            }
+            else
+            {
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
+            Console.WriteLine();
+        }
+
+        private static void ParseFromStrings()
+        {
+            Console.WriteLine("=. Data type parsing:");
+            bool b = bool.Parse("True");
+            Console.WriteLine("Value of b: {0}",b);
+            double d = double.Parse("99.884");
+            Console.WriteLine("Value of d: {0}", d);
+            int i = int.Parse("8");
+            Console.WriteLine("Value of i: {0}", i);
+            char c = Char.Parse("w");
+            Console.WriteLine("Value of c: {0}", c);
+            Console.WriteLine();
         }
 
         // Members of System.Char
