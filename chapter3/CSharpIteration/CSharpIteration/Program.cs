fdf973a043c15599e0fd8db248ff6095b44fb4df
diwashrestha <diwash712@gmail.com>
2020-06-05 23:14:59 +0545

    Add Switch statement

--- chapter3/CSharpIteration/CSharpIteration/Program.cs
+++ chapter3/CSharpIteration/CSharpIteration/Program.cs
@@ -13,6 +13,95 @@ namespace CSharpIteration
             LinqQueryOverInts();
             WhileLoopExample();
             DoWhileLoopExample();
+            IfElseExample();
+            ExecuteIfElseUsingConditionalOperator();
+            SwitchOnStringExample();
+            SwitchOnEnumExample();
+            Console.WriteLine();
+        }
+
+        private static void SwitchOnEnumExample()
+        {
+            Console.Write("Enter your favorite day of the week: ");
+            DayOfWeek favDay;
+            try
+            {
+                favDay = (DayOfWeek)Enum.Parse(typeof(DayOfWeek), Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad input!");
+                return;
+            }
+            switch (favDay)
+            {
+                case DayOfWeek.Sunday:
+                    Console.WriteLine("Football!!");
+                    break;
+                case DayOfWeek.Monday:
+                    Console.WriteLine("Another day, another dollar");
+                    break;
+                case DayOfWeek.Tuesday:
+                    Console.WriteLine("At least it is not Monday");
+                    break;
+                case DayOfWeek.Wednesday:
+                    Console.WriteLine("A fine day.");
+                    break;
+                case DayOfWeek.Thursday:
+                    Console.WriteLine("Almost Friday...");
+                    break;
+                case DayOfWeek.Friday:
+                    Console.WriteLine("Yes, Friday rules!");
+                    break;
+                case DayOfWeek.Saturday:
+                    Console.WriteLine("Great day indeed.");
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        private static void SwitchOnStringExample()
+        {
+            Console.WriteLine("C# or VB");
+            Console.WriteLine("Please pick your language preference");
+
+            string langChoice = Console.ReadLine();
+            switch (langChoice)
+            {
+                case "C#":
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                case "VB":
+                    Console.WriteLine("VB: OOP, multithreading and more!");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+
+            }
+            Console.WriteLine();
+        }
+
+        private static void ExecuteIfElseUsingConditionalOperator()
+        {
+            string stringData = "My textual data";
+            Console.WriteLine(stringData.Length > 0
+                ? "string is greater than 0 characters"
+                : "string is not greater than 0 characters");
+            Console.WriteLine();
+        }
+
+        private static void IfElseExample()
+        {
+            string stringData = "My textual data";
+            if (stringData.Length > 0)
+            {
+                Console.WriteLine("string is greater than 0 characters");
+            }
+            else
+            {
+                Console.WriteLine("string is not greater than 0 characters");
+            }
             Console.WriteLine();
         }
 
