ab187600dc011fb237cc3e31f081f1de3ca3791e
diwashrestha <diwash712@gmail.com>
2020-06-05 11:06:48 +0545

    String Builder

--- chapter3/BasicConsoleIO/BasicConsoleIO/Program.cs
+++ chapter3/BasicConsoleIO/BasicConsoleIO/Program.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.Linq.Expressions;
 using System.Numerics;
+using System.Text;
 
 namespace BasicConsoleIO
 {
@@ -30,8 +31,25 @@ namespace BasicConsoleIO
             StringEquality();
             StringEqualitySpecifyingCompareRules();
             StringsAreImmutable();
+            FunWithStringBuilder();
             Console.ReadLine();
 
+        }
+
+        private static void FunWithStringBuilder()
+        {
+            Console.WriteLine("=> Using the StringBuilder:");
+            StringBuilder sb = new StringBuilder("**** Fantastic Games ****");
+            sb.Append("\n");
+            sb.AppendLine("Half Life");
+            sb.AppendLine("Morrowind");
+            sb.AppendLine("Deus Ex" + "2");
+            sb.AppendLine("System Shock");
+            Console.WriteLine(sb.ToString());
+            sb.Replace("2", "Invisible War");
+            Console.WriteLine(sb.ToString());
+            Console.WriteLine("sb has {0} chars.", sb.Length);
+            Console.WriteLine();
         }
 
         private static void StringsAreImmutable()
