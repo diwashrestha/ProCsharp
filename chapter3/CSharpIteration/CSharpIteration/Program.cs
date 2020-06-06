413360f36f64b94cb22efc40af8599173759cb79
diwashrestha <diwash712@gmail.com>
2020-06-06 13:44:02 +0545

    ExecutePatternMatchingSwitchWithWhen

--- chapter3/CSharpIteration/CSharpIteration/Program.cs
+++ chapter3/CSharpIteration/CSharpIteration/Program.cs
@@ -19,6 +19,30 @@ namespace CSharpIteration
             SwitchOnEnumExample();
             SwitchWithGoto();
             ExecutePatternMatchingSwitch();
+            ExecutePatternMatchingSwitchWithWhen();
+            Console.WriteLine();
+        }
+
+        private static void ExecutePatternMatchingSwitchWithWhen()
+        {
+            Console.WriteLine("1 [C#], 2 [VB]");
+            Console.Write("Please pick your language preference: ");
+            object langChoice = Console.ReadLine();
+            var choice = int.TryParse(langChoice.ToString(), out int c) ? c : langChoice;
+            switch (choice)
+            {
+                case int i when i == 2:
+                case string s when s.Equals("VB", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("VB: OOP, multithreading, and more!");
+                    break;
+                case int i when i == 1:
+                case string s when s.Equals("C#", StringComparison.OrdinalIgnoreCase):
+                    Console.WriteLine("Good choice, C# is a fine language.");
+                    break;
+                default:
+                    Console.WriteLine("Well...good luck with that!");
+                    break;
+            }
             Console.WriteLine();
         }
 
