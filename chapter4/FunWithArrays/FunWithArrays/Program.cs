ab89d3127d24062bb40358459d9ec3b696c0fe96
diwashrestha <diwash712@gmail.com>
2020-06-06 14:48:46 +0545

    Add Jagged Array

--- chapter4/FunWithArrays/FunWithArrays/Program.cs
+++ chapter4/FunWithArrays/FunWithArrays/Program.cs
@@ -13,7 +13,31 @@ namespace FunWithArrays
             DeclareImplicitArrays();
             ArrayOfObjects();
             RectMultiDimensionalArray();
+            JaggedMultidimensionalArray();
             Console.ReadLine();
+        }
+
+        private static void JaggedMultidimensionalArray()
+        {
+            Console.WriteLine("=> Jagged multidimensional array.");
+            // A jagged MD array (i.e., an array of arrays).
+            // Here we have an array of 5 different arrays.
+            int[][] myJagArray = new int[5][];
+
+            // Create the jagged array.
+            for (int i = 0; i < myJagArray.Length; i++)
+                myJagArray[i] = new int[i + 7];
+
+
+            // Print each row (remember, each element is defaulted to zero!).
+            for (int i = 0; i < 5; i++)
+            {
+                for(int j = 0; j < myJagArray[i].Length; j++)
+                    Console.Write(myJagArray[i][j] + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine(myJagArray[2].Length);
         }
 
         private static void RectMultiDimensionalArray()
