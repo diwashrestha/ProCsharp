e795902a3bd33baae8845c0f3c07fe7457c24a9b
diwashrestha <diwash712@gmail.com>
2020-06-10 14:19:28 +0545

    System.Array Base Class

--- chapter4/FunWithArrays/FunWithArrays/Program.cs
+++ chapter4/FunWithArrays/FunWithArrays/Program.cs
@@ -14,10 +14,46 @@ namespace FunWithArrays
             ArrayOfObjects();
             RectMultiDimensionalArray();
             JaggedMultidimensionalArray();
-            //PrintArray();
-            //GetStringArray();
             PassAndReceiveArrays();
+            SystemArrayFunctionality();
             Console.ReadLine();
+        }
+
+        private static void SystemArrayFunctionality()
+        {
+            Console.WriteLine("=> Working with System.Array.");
+            // Initialize items at startup.
+            string[] gothicBands = {"Tones on Tail", "Bauhaus", "Sisters of Mercy"};
+
+            // Print out names in declared order.
+            Console.WriteLine("-> Here is the array:");
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                // Print a name
+                Console.Write(gothicBands[i] + ", ");
+            }
+            Console.WriteLine("\n");
+
+            // Reverse them...
+            Array.Reverse(gothicBands);
+            Console.WriteLine("-> The reversed array");
+
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                // Print a name.
+                Console.Write(gothicBands[i] + ", ");
+            }
+            Console.WriteLine("\n");
+
+            // Clear out all but the first member.
+            Console.WriteLine("-> Cleared out all but one...");
+            Array.Clear(gothicBands, 1, 2);
+
+            for (int i = 0; i < gothicBands.Length; i++)
+            {
+                Console.Write(gothicBands[i] + ", ");
+            }
+            Console.WriteLine();
         }
 
         private static void PassAndReceiveArrays()
