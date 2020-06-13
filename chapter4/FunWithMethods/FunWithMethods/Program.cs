e53bd775b73482bff82bf82d7a1546a61ca26216
diwashrestha <diwash712@gmail.com>
2020-06-13 20:03:29 +0545

    Add  Enter logdata

--- chapter4/FunWithMethods/FunWithMethods/Program.cs
+++ chapter4/FunWithMethods/FunWithMethods/Program.cs
@@ -18,7 +18,7 @@ namespace FunWithMethods
             // No need to assign initial value to local variables
             // used as output parameters provided the first time
             // you use them ia as output arguments.
-            // C# 7 allows for out parameters to be declared in th metho calll
+            // C# 7 allows for out parameters to be declared in th method call
             Add(90, 90, out int ans);
             Console.WriteLine("90 + 90 = {0}", ans);
 
@@ -30,8 +30,42 @@ namespace FunWithMethods
             Console.WriteLine("Int is: {0}", i);
             Console.WriteLine("String is: {0}",str);
             Console.WriteLine("Boolean is:{0}", b);
+
+            // Pass in a comma-delimited list of doubles...
+            double average;
+            average = CalculateAverage(4.0, 3.2, 5.7, 64.22, 87.2);
+            Console.WriteLine("Average of data is: {0}", average);
+
+            // or pass an array of doubles.
+            double[] data = {4.0, 3.2, 5.7};
+            average = CalculateAverage(data);
+            Console.WriteLine("Average of data is: {0}", average);
+
+            EnterLogData("Oh no! Grid can't find data");
+            EnterLogData("Oh no! I can't find the payroll data", "CFO");
             Console.ReadLine();
 
+        }
+
+        private static void EnterLogData(string message, string owner = "Programmer", DateTime timeStamp = default)
+        {
+            Console.Beep();
+            Console.WriteLine("Error: {0}", message);
+            Console.WriteLine("Owner of Error: {0}", owner);
+            Console.WriteLine("Time of Error: {0}", timeStamp);
+            Console.WriteLine();
+        }
+
+        private static double CalculateAverage(params double[] values)
+        {
+            Console.WriteLine("You sent me {0} doubles.", values.Length);
+
+            double sum = 0;
+            if (values.Length == 0)
+                return sum;
+            for (int i = 0; i < values.Length; i++)
+                sum += values[i];
+            return (sum / values.Length);
         }
 
         private static void FillTheseValues(out int a, out string b, out bool c)
