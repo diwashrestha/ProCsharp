132a57ae1df77adaba36aae548602a6bfc91128b
diwashrestha <diwash712@gmail.com>
2020-06-15 09:20:28 +0545

    Create Car Class

--- chapter5/SimpleClassExample/SimpleClassExample/Car.cs
+++ chapter5/SimpleClassExample/SimpleClassExample/Car.cs
@@ -6,5 +6,16 @@ namespace SimpleClassExample
 {
     class Car
     {
+        // The 'state' of the Car
+        public string petName;
+        public int currSpeed;
+
+        // The Functionality of the car.
+        // Using the expression-bodied member syntax introduced in c# 6
+        public void PrintState()
+            => Console.WriteLine("{0} is going {1} MPH.", petName, currSpeed);
+
+        public void SpeedUp(int delta)
+            => currSpeed += delta;
     }
 }
--- chapter5/SimpleClassExample/SimpleClassExample/Program.cs
+++ chapter5/SimpleClassExample/SimpleClassExample/Program.cs
@@ -6,7 +6,22 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           Console.WriteLine("**** Fun with Class Types ****\n");
+
+           // Allocate and configure a Car object.
+           Car myCar = new Car();
+           myCar.petName = "Henry";
+           myCar.currSpeed = 10;
+
+           // Speed up the car a few times and print out the 
+           // new state
+           for (int i = 0; i <= 10; i++)
+           {
+               myCar.SpeedUp(5);
+               myCar.PrintState();
+           }
+
+           Console.ReadLine();
         }
     }
 }
