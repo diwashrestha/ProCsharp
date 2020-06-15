400690504cd5511363bc3f13edac067b633330ca
diwashrestha <diwash712@gmail.com>
2020-06-15 18:38:20 +0545

    ADD static Data and Method

--- chapter5/StaticDataAndMembers/StaticDataAndMembers/Program.cs
+++ chapter5/StaticDataAndMembers/StaticDataAndMembers/Program.cs
@@ -6,7 +6,17 @@ namespace StaticDataAndMembers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Static Data ****\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+
+            // Print the current interest rate.
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+
+            // Make new Object, this does NOT 'reset' the interest rate.
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            Console.WriteLine("Interest Rate is: {0}", SavingsAccount.GetInterestRate());
+            Console.ReadLine();
         }
     }
 }
---
+++ chapter5/StaticDataAndMembers/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        // Instance-level data.
+        public double currBalance;
+
+        // A static point of data.
+        public static double currInterestRate = 0.04;
+
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+
+        // Static members to get/set interest rate.
+        public static void SetInterestRate(double newRate)
+        {
+            currInterestRate = newRate;
+        }
+
+        public static double GetInterestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
