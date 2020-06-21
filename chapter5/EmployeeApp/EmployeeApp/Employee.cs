8e7774da0933516e2c85a250650be88c583d1ead
diwashrestha <diwash712@gmail.com>
2020-06-21 21:25:13 +0545

    Add Encapsulation

--- chapter5/EmployeeApp/EmployeeApp/Employee.cs
+++ chapter5/EmployeeApp/EmployeeApp/Employee.cs
@@ -6,5 +6,51 @@ namespace EmployeeApp
 {
     class Employee
     {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        // Accessor (get method).
+        public string GetName()
+        {
+            return empName;
+        }
+
+        // Mutator (set method).
+        public void SetName(string name)
+        {
+            // Do a check on incoming value
+            // before making assignment.
+            if (name.Length > 15)
+                Console.WriteLine("Error! Name length exceeds 15 characters!");
+            else
+            {
+                empName = name;
+            }
+        }
+
+        // Constructors.
+        public Employee() {}
+
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", empName);
+            Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Pay: {0}", currPay);
+        }
     }
 }
--- chapter5/EmployeeApp/EmployeeApp/Program.cs
+++ chapter5/EmployeeApp/EmployeeApp/Program.cs
@@ -6,7 +6,15 @@ namespace EmployeeApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Encapsulation *****\n");
+            Employee emp = new Employee("Marvin", 456, 30_000);
+            emp.GiveBonus(1000);
+            emp.DisplayStats();
+
+            // Use the get/set methods to interact with the object's name.
+            emp.SetName("Marv");
+            Console.WriteLine("Employee is named: {0}", emp.GetName());
+            Console.ReadLine();
         }
     }
 }
