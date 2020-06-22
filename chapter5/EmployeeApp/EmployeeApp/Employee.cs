70a3e8f0dc947613829d0d06bc2402bfc27589ca
diwashrestha <diwash712@gmail.com>
2020-06-22 13:55:18 +0545

    Encapsulation using .net properties

--- chapter5/EmployeeApp/EmployeeApp/Employee.cs
+++ chapter5/EmployeeApp/EmployeeApp/Employee.cs
@@ -10,33 +10,54 @@ namespace EmployeeApp
         private string empName;
         private int empID;
         private float currPay;
+        private int empAge;
 
         // Accessor (get method).
-        public string GetName()
+        public string Name
         {
-            return empName;
-        }
-
-        // Mutator (set method).
-        public void SetName(string name)
-        {
-            // Do a check on incoming value
-            // before making assignment.
-            if (name.Length > 15)
-                Console.WriteLine("Error! Name length exceeds 15 characters!");
-            else
+            get { return empName; }
+            set
             {
-                empName = name;
+                if(value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                {
+                    empName = value;
+                }
             }
         }
+
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+
 
         // Constructors.
         public Employee() {}
 
         public Employee(string name, int id, float pay)
+        :this(name, 0, id, pay){}
+
+        public Employee(string name, int age, int id, float pay)
         {
             empName = name;
             empID = id;
+            empAge = age;
             currPay = pay;
         }
 
@@ -50,6 +71,7 @@ namespace EmployeeApp
         {
             Console.WriteLine("Name: {0}", empName);
             Console.WriteLine("ID: {0}", empID);
+            Console.WriteLine("Age: {0}", empAge);
             Console.WriteLine("Pay: {0}", currPay);
         }
     }
--- chapter5/EmployeeApp/EmployeeApp/Program.cs
+++ chapter5/EmployeeApp/EmployeeApp/Program.cs
@@ -7,16 +7,15 @@ namespace EmployeeApp
         static void Main(string[] args)
         {
             Console.WriteLine("***** Fun with Encapsulation *****\n");
-            Employee emp = new Employee("Marvin", 456, 30_000);
+            Employee emp = new Employee("Marvin", 20, 456, 30_000);
+            emp.Age++;
             emp.GiveBonus(1000);
             emp.DisplayStats();
 
-            Employee emp2 = new Employee();
-            emp2.SetName("Xena the warrior princess");
+            // Reset and then get the Name property.
+            emp.Name = "Marc";
+            Console.WriteLine("Employee is named: {0}", emp.Name);
 
-            // Use the get/set methods to interact with the object's name.
-            emp.SetName("Marv");
-            Console.WriteLine("Employee is named: {0}", emp.GetName());
             Console.ReadLine();
         }
     }
