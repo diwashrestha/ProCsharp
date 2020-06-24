eb0238f06cceb2f613a363605cd7259b8d55aca3
diwashrestha <diwash712@gmail.com>
2020-06-24 21:52:41 +0545

    Static Read Only Fields

---
+++ chapter5/ConstData/ConstData/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        public static readonly double PI = 3.14;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Const *****\n");
+            Console.WriteLine("The value of PI is: {0}", MyMathClass.PI);
+
+            Console.ReadLine();
+        }
+    }
+}
