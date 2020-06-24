e8dcca3696caa87d0bea2da5409c78ac2b5430bd
diwashrestha <diwash712@gmail.com>
2020-06-24 18:16:40 +0545

    Add Automatic Properties

---
+++ chapter5/AutoProps/AutoProps/Garage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutoProps
+{
+    class Garage
+    {
+        // The hidden int backing field is set to zero!
+        public int NumberOfCars { get; set; }
+
+        // The hidden Car backing field is set to null!
+        public Car MyAuto { get; set; }
+
+        // Must use constructors to override default
+        // values asigned to hidden backing fields.
+        public Garage()
+        {
+            MyAuto = new Car();
+            NumberOfCars = 1;
+        }
+
+        public Garage(Car car, int number)
+        {
+            MyAuto = car;
+            NumberOfCars = number;
+        }
+    }
+}
---
+++ chapter5/AutoProps/AutoProps/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AutoProps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Fun with Automatic Properties *****\n");
+
+            Car c = new Car();
+            c.PetName = "Frank";
+            c.Speed = 55;
+            c.Color = "Red";
+
+            Console.WriteLine("Your car is named {0}? That's odd...", c.PetName);
+            c.DisplayStats();
+
+            Garage g = new Garage();
+            g.MyAuto = c;
+            // OK, prints default value of zero.
+            Console.WriteLine("Number of Cars: {0}", g.NumberOfCars);
+
+            // Runtime error! Backing field is currently null!
+            Console.WriteLine(g.MyAuto.PetName);
+            Console.ReadLine();
+        }
+    }
+}
