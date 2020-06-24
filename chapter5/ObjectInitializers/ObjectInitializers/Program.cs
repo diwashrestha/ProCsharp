b447f27b8447fd15f394c8fe801058baeba55f94
diwashrestha <diwash712@gmail.com>
2020-06-24 19:55:01 +0545

    Calling Custom Constructors with Initialization Syntax

--- chapter5/ObjectInitializers/ObjectInitializers/Program.cs
+++ chapter5/ObjectInitializers/ObjectInitializers/Program.cs
@@ -6,7 +6,25 @@ namespace ObjectInitializers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Object Init Syntax *****\n");
+
+            // Make aPoint by setting each property manually.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+
+            // Or make a Point via a custom constructor.
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+
+            // Or make a Point using object init syntax.
+            Point finalPoint = new Point {X = 30, Y = 30};
+            finalPoint.DisplayStats();
+
+            Point goldPoint = new Point(PointColor.Gold){X = 90, Y = 20};
+            goldPoint.DisplayStats();
+            Console.ReadLine();
         }
     }
 }
