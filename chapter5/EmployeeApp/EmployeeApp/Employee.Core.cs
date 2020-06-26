e8530d0ce37a085f9b5f58b8d3556b790e1b163b
diwashrestha <diwash712@gmail.com>
2020-06-26 12:39:36 +0545

    Understanding Inheritance

--- chapter5/EmployeeApp/EmployeeApp/Employee.Core.cs
+++ chapter5/EmployeeApp/EmployeeApp/Employee.Core.cs
@@ -11,5 +11,23 @@ namespace EmployeeApp
         private int empID;
         private float currPay;
         private int empAge;
+
+
+
+
+        // Constructors.
+        public Employee() { }
+
+        public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay) { }
+
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+            Pay = pay;
+        }
+
     }
 }
---
+++ chapter6/BasicInheritance/BasicInheritance/MiniVan.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BasicInheritance
+{
+    class MiniVan: Car
+    {
+    }
+}
