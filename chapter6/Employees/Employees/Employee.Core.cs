0715c10ef5c8f175e29c225a77217e2493794ef9
diwashrestha <diwash712@gmail.com>
2020-06-26 14:17:45 +0545

    Add New Employees App

---
+++ chapter6/Employees/Employees/Employee.Core.cs
@@ -0,0 +1,29 @@
+﻿namespace Employees
+{
+    partial class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+
+
+
+
+        // Constructors.
+        public Employee() { }
+
+        public Employee(string name, int id, float pay)
+            : this(name, (int) 0, id, pay) { }
+
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            ID = id;
+            Age = age;
+            Pay = pay;
+        }
+
+    }
+}
---
+++ chapter6/Employees/Employees/Employee.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Employees
+{
+    partial class Employee
+    {
+
+        // Methods.
+        public void GiveBonus(float amount)
+        {
+            Pay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+        }
+
+        // Properties as before
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error! Name length exceeds 15 characters!");
+                else
+                {
+                    empName = value;
+                }
+            }
+        }
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+    }
+}
+
