4826106de992cac30e2e8464f1770c2c2de95d66
diwashrestha <diwash712@gmail.com>
2020-06-28 20:15:05 +0545

    Member Shadowing

--- chapter6/Shapes/Shapes/Program.cs
+++ chapter6/Shapes/Shapes/Program.cs
@@ -6,7 +6,36 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Polymorphism *****\n");
+
+            // Make an array of Shape- Compatible objects.
+            Shape[] myShapes =
+            {
+                new Hexagon(), new Circle(), new Hexagon("Mick"),
+                new Circle("Diwash"), new Hexagon("Lynda")
+            };
+
+            // Loop over each item and interact with the
+            // Polymorphic interface.
+            foreach (Shape s in myShapes)
+            {
+                s.Draw();
+            }
+
+            Console.ReadLine();
+
+            Hexagon hex = new Hexagon("Beth");
+            hex.Draw();
+            Circle cir = new Circle("Cindy");
+            cir.Draw();
+
+            // This calls the Draw() method of the ThreeDCircle.
+            ThreeDCircle o = new ThreeDCircle();
+            o.Draw();
+
+            // This calls the Draw() method of the parent!
+            ((Circle)o).Draw();
+            Console.ReadLine();
         }
     }
 }
