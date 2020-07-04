bc17d7b003109fba2099cfff2c16ae839ba452fc
diwashrestha <diwash712@gmail.com>
2020-07-04 16:55:12 +0545

    Building Custom Exceptions, Take 2

--- chapter7/CustomException/CustomException/CarIsDeadException.cs
+++ chapter7/CustomException/CustomException/CarIsDeadException.cs
@@ -6,20 +6,16 @@ namespace CustomException
 {
     class CarIsDeadException: ApplicationException
     {
-        private string messageDetails = String.Empty;
         public DateTime ErrorTimeStamp { get; set; }
         public string CauseOfError { get; set; }
 
         public CarIsDeadException() { }
 
-        public CarIsDeadException(string message, string cause, DateTime time)
+        public CarIsDeadException(string message, string cause, DateTime time): base(message)
         {
-            messageDetails = message;
             CauseOfError = cause;
             ErrorTimeStamp = time;
         }
 
-        // Override the Exception.Message property.
-        public override string Message => $"Car Error Message: {messageDetails}";
     }
 }
