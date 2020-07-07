587a32541051a3eaada341e3f630dfb894b87a7c
diwashrestha <diwash712@gmail.com>
2020-07-07 20:06:16 +0545

    Created Coffee Machine

---
+++ chapter7/Coffee Machine/Coffee Machine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Coffee_Machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
