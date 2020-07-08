4008d7dd08f64383019165c42415c9908efe5d93
diwashrestha <diwash712@gmail.com>
2020-07-08 19:44:17 +0545

    Add CoffeeItems calculator

--- chapter7/Coffee Machine/Coffee Machine/Program.cs
+++ chapter7/Coffee Machine/Coffee Machine/Program.cs
@@ -6,7 +6,33 @@ namespace Coffee_Machine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Stage 1
+            //Console.WriteLine("Starting to make a coffee");
+            //Console.WriteLine("Grinding coffee beans");
+            //Console.WriteLine("Boiling water");
+            //Console.WriteLine("Mixing boiled water with crushed coffee beans");
+            //Console.WriteLine("Pouring coffee into the cup");
+            //Console.WriteLine("Pouring some milk into the cup");
+            //Console.WriteLine("Coffee is ready!");
+
+            Console.WriteLine("Write how many cups of coffee you will need: ");
+            int coffeeCup = Convert.ToInt32(Console.ReadLine());
+
+            coffeeItemsCalculator(coffeeCup);
+        }
+
+        public static void coffeeItemsCalculator(int coffeeCup)
+        {
+            int waterAmount = 200 * coffeeCup;
+            int milkAmount = 50 * coffeeCup;
+            int coffeeBean = 15 * coffeeCup;
+
+            Console.WriteLine("For {0} cups of coffee you will need:", coffeeCup);
+            Console.WriteLine("{0} ml of water", waterAmount);
+            Console.WriteLine("{0} ml of milk", milkAmount);
+            Console.WriteLine("{0} g of coffee beans", coffeeBean);
+
+
         }
     }
 }
