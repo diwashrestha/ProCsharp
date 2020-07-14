ca9354e034aaf7b594e81ee33a65703aff79da82
diwashrestha <diwash712@gmail.com>
2020-07-14 22:33:38 +0545

    Started interface

---
+++ chapter8/CustomInterface/CustomInterface/Shape.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomInterface
+{
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        {
+            PetName = name;
+        }
+        public string PetName { get; set; }
+
+        // A single virtual method.
+        public abstract void Draw();
+
+    }
+}
